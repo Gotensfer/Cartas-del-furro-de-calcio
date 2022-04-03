9234b7d04fd7911ec476f4c15eb6fde52801e5ce
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-04-02 20:52:01 -0500

    Añadida lógica pruebas unitarias #1 y #2

--- PruebasUnitarias/UnitTest1.cs
+++ PruebasUnitarias/UnitTest1.cs
@@ -6,9 +6,15 @@ namespace PruebasUnitarias
     public class DeckFunctionality
     {
         [TestMethod]
-        public void test4()
+        public void CreatedDeck_Has_DefinedMaxCP()
         {
+            // Vector de prueba: 
+            const int expectedDeckCP = 40;
 
+            // - Crear un deck
+
+            // - Comparar el CP máximo del Deck con el CP esperado
+            // - Si el CP máximo del Deck es igual al CP esperado se pasa la prueba
         }
     }
 
@@ -16,9 +22,19 @@ namespace PruebasUnitarias
     public class BaseCard
     {
         [TestMethod]
-        public void test3()
+        public void AllCardTypes_Has_BasicChacteristics()
         {
+            // Vector de prueba: 
+            const string notExpectedName = "";
+            const int notExpectedRarity = 0;
+            const int notExpectedCP = 0;
 
+            // - Crear una carta de Character
+            // - Crear una carta de Equip
+            // - Crear una carta de Support skill
+
+            // - Comparar el name, rarity y cp de la carta una de las cartas con los vectores de prueba
+            // - Si se satisfacen los vectores de prueba se pasa la prueba
         }
     }
 
