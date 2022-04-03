ecac662544b1491f7a432231e1435914162c0a62
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-04-02 19:03:08 -0500

    Creación de la solución y proyectos

---
+++ CartasDelFurroDeCalcio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CartasDelFurroDeCalcio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
