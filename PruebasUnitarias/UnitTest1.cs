247cbe8e4b64f149bff18ce19a259af7e8f0cbc0
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-04-02 19:47:37 -0500

    Organización de las clases de pruebas unitarias

--- PruebasUnitarias/UnitTest1.cs
+++ PruebasUnitarias/UnitTest1.cs
@@ -3,11 +3,52 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PruebasUnitarias
 {
     [TestClass]
-    public class UnitTest
+    public class DeckFunctionality
+    {
+        [TestMethod]
+        public void test4()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class BaseCard
+    {
+        [TestMethod]
+        public void test3()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class CharacterCardFunctionality
     {
         [TestMethod]
         public void TestMethod1()
         {
+
+        }
+    }
+
+    [TestClass]
+    public class EquipCardFunctionality
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class SupportSkillCardFunctionality
+    {
+        [TestMethod]
+        public void test2()
+        {
+
         }
     }
 }
