4458db6fd543aa2a590582e786e93822f44a65ad
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-04-05 01:34:15 -0500

    Prueba unitaria #2 Terminada

--- PruebasUnitarias/UnitTest1.cs
+++ PruebasUnitarias/UnitTest1.cs
@@ -33,13 +33,41 @@ namespace PruebasUnitarias
             const Rarity notExpectedRarity = Rarity.None;
             const int notExpectedCP = 0;
 
-            
+            // - Crear cartas "vacias" de los 3 tipos
+            Character character0 = new Character("", Rarity.None, 0, Affinity.None, 0, 0);
+            Equipment equipment0 = new Equipment("", Rarity.None, 0, Affinity.None, TargetAttribute.None, 0);
+            SupportSkill supportSkill0 = new SupportSkill("", Rarity.None, 0, EffectType.None, 0);
 
-            // - Crear una carta de Character
-            // - Crear una carta de Equip
-            // - Crear una carta de Support skill
+            // - Crear una carta "estandar" de los 3 tipos
+            Character character1 = new Character("Name0", Rarity.Common, 1, Affinity.Knight, 1, 1);
+            Equipment equipment1 = new Equipment("Name1", Rarity.Rare, 1, Affinity.Knight, TargetAttribute.AP, 1);
+            SupportSkill supportSkill1 = new SupportSkill("Name2", Rarity.SuperRare, 1, EffectType.ReduceRP, 1);
 
-            // - Comparar el name, rarity y cp de la carta una de las cartas con los vectores de prueba
+            // - Comparar el name, rarity y cp de la carta una de las cartas estandar con los vectores de prueba
+            Assert.AreNotEqual(character0.Name, notExpectedName);
+            Assert.AreNotEqual(character0.Rarity, notExpectedRarity);
+            Assert.AreNotEqual(character0.Cp, notExpectedCP);
+
+            Assert.AreNotEqual(equipment0.Name, notExpectedName);
+            Assert.AreNotEqual(equipment0.Rarity, notExpectedRarity);
+            Assert.AreNotEqual(equipment0.Cp, notExpectedCP);
+
+            Assert.AreNotEqual(supportSkill0.Name, notExpectedName);
+            Assert.AreNotEqual(supportSkill0.Rarity, notExpectedRarity);
+            Assert.AreNotEqual(supportSkill0.Cp, notExpectedCP);
+
+            // - Comparar el name, rarity y cp de la carta una de las cartas estandar con los vectores de prueba
+            Assert.AreNotEqual(character1.Name, notExpectedName);
+            Assert.AreNotEqual(character1.Rarity, notExpectedRarity);
+            Assert.AreNotEqual(character1.Cp, notExpectedCP);
+
+            Assert.AreNotEqual(equipment1.Name, notExpectedName);
+            Assert.AreNotEqual(equipment1.Rarity, notExpectedRarity);
+            Assert.AreNotEqual(equipment1.Cp, notExpectedCP);
+
+            Assert.AreNotEqual(supportSkill1.Name, notExpectedName);
+            Assert.AreNotEqual(supportSkill1.Rarity, notExpectedRarity);
+            Assert.AreNotEqual(supportSkill1.Cp, notExpectedCP);
             // - Si se satisfacen los vectores de prueba se pasa la prueba
         }
     }
