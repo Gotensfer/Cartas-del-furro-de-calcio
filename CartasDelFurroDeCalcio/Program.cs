abb29bd7ac3998c14e7b265176913a6e93fa5b88
Camilo Correa Rojas <camilo.correar@upb.edu.co>
2022-04-05 23:55:57 -0500

    Gachadolor

--- CartasDelFurroDeCalcio/Program.cs
+++ CartasDelFurroDeCalcio/Program.cs
@@ -3,10 +3,14 @@
 namespace CartasDelFurroDeCalcio
 {
     class Program
-    {
+    {   
+        
         static void Main(string[] args)
         {
+            Gachapon gacha = new Gachapon();
             Console.WriteLine("Hello World!");
+
+
         }
     }
 }
